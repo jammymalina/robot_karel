668d2d6e4547e30343a41bdbef69a0a2f87ebe27
jammymalina <jakub.jantosik@gmail.com>
2023-07-16 22:50:11 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Navbar from '../lib/components/NavigationPanel.svelte';
+</script>
+
+<div class="flex flex-col h-screen">
+	<Navbar />
+	<main class="flex flex-grow">
+		<slot />
+	</main>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      'theme-white': '#eeeeee',
+      'theme-teal': '#00adb5',
+      'theme-light-gray': '#393e46',
+      'theme-dark-gray': '#222831',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
