9abbbe541fb6c8c10b00e063b24476da0bcf85a7
jammymalina <jakub.jantosik@gmail.com>
2023-07-18 23:54:36 +0200

    Update

--- src/lib/components/NavigationPanel.svelte
+++ src/lib/components/NavigationPanel.svelte
@@ -1,5 +1,5 @@
 <nav class="bg-theme-teal text-theme-dark-gray">
   <div class="flex flex-wrap items-center justify-between mx-auto p-4">
-    <span class="self-center text-2xl font-semibold whitespace-nowrap"> Robot Karel </span>
+    <span class="self-center text-2xl font-semibold whitespace-nowrap"> WebGPU gems </span>
   </div>
 </nav>
